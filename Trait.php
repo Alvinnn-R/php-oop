4bbbf9dd85b6b93e9d58fe3792e56ed9c1d1868a
Alvin <alvinramasaputra03@gmail.com>
2025-02-12 20:57:46 +0700

    learn oop php : Trait and Trait Properti

---
+++ Trait.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once 'data/SayGoodBye.php';
+
+use Data\Traits\Person;
+
+$person = new Person();
+$person->hello("Alvin");
+$person->goodBye("Rama");
+
+$person->name = "saputra";
---
+++ data/SayGoodBye.php
@@ -0,0 +1,37 @@
+<?
+namespace Data\Traits;
+
+trait sayGoodBye
+{
+    public function goodBye(?string $name): void
+    {
+        if (is_null($name)) {
+            echo "Good Bye" . PHP_EOL;
+        } else {
+            echo "Good Bye $name" . PHP_EOL;
+        }
+    }
+}
+
+trait sayHello
+{
+    public function hello(?string $name): void
+    {
+        if (is_null($name)) {
+            echo "Hello" . PHP_EOL;
+        } else {
+            echo "Hello $name" . PHP_EOL;
+        }
+    }
+}
+
+// trait properti
+trait HasName
+{
+    public string $name;
+}
+
+class Person
+{
+    use sayHello, sayGoodBye, HasName;
+}
